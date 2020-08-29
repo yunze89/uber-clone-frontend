fa6b8f4dd1c3c4eaf0d3e0dc4d9902afa06ff8a3
yunzema <yunze89@gmail.com>
2020-08-29 15:01:39 +0900

    init & add apollo client instance

---
+++ src/apollo.ts
@@ -0,0 +1,57 @@
+import ApolloClient, { Operation } from "apollo-boost";
+
+//graphql client인 apollo client 객체 생성
+const apolloClient = new ApolloClient({
+  uri: "http://localhot:4000/graphql", //backend graphql endpoint
+  //로그인 여부에 대해 global state 관리위해 설정
+  clientState: {
+    defaults: {
+      auth: {
+        __typename: "Auth",
+        isLoggedIn: Boolean(localStorage.getItem("jwt")),
+      },
+    },
+  },
+  //header에 jwt 설정
+  request: async (operation: Operation) => {
+    operation.setContext({
+      headers: {
+        "X-JWT": localStorage.getItem("jwt"),
+      },
+    });
+  },
+
+  //서버와는 별개로 clientState를 조작하기 위한 resolver/mutation
+  resolvers: {
+    Mutation: {
+      logUserIn: (_, { token }, { cache }) => {
+        localStorage.setItem("jwt", token); //localstorage에 jwt 토큰값 저장
+        cache.writeData({
+          //앱에서 사용
+          data: {
+            auth: {
+              __typename: "Auth",
+              isLoggedIn: true,
+            },
+          },
+        });
+        return null;
+      },
+      logUserOut: (_, __, { cache }) => {
+        localStorage.removeItem("jwt");
+        cache.writeData({
+          //앱에서 사용
+          data: {
+            auth: {
+              __typename: "Auth",
+              isLoggedIn: false,
+            },
+          },
+        });
+        return null;
+      },
+    },
+  },
+});
+
+export default apolloClient;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { ApolloProvider } from "react-apollo";
+import apolloClient from "./apollo";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* provider에 apolloclient 객체를 넣어주어 연결시켜준다 */}
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
